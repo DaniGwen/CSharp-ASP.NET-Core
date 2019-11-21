e6871adb321d1430d189c991f901c3c2a75c4460
Daniel <DaniGwen@users.noreply.github.com>
2019-11-21 05:10:27 -0800

    FDMC update

---
+++ FluffyDuffyMunchkinCats/Data/Context/DbConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class DbConfig
+    {
+        public const string SqlConnection = @"Server=LAPTOP-BDSBIU1R\SQLEXPRESS;Database=FDMC;Integrated Security=true";
+    }
+}
---
+++ FluffyDuffyMunchkinCats/Data/Context/FDMCContext.cs
@@ -0,0 +1,25 @@
+﻿using Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Data.DbContext
+{
+    public class FDMCContext : DbContext
+    {
+        public FDMCContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbConfig.SqlConnection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
