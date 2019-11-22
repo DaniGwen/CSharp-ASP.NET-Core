b938e487935d2e86504d601fb9c189235a04307f
Daniel <DaniGwen@users.noreply.github.com>
2019-11-21 21:30:14 -0800

    Update

--- FluffyDuffyMunchkinCats/FDMC/Startup.cs
+++ FluffyDuffyMunchkinCats/FDMC/Startup.cs
@@ -24,6 +24,7 @@ namespace FDMC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,16 +43,9 @@ namespace FDMC
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseMvcWithDefaultRoute();
 
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
         }
     }
 }
