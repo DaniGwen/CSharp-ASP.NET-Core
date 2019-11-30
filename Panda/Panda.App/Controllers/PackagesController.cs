5a44457d3001637559ab60827bcc17a85630ab25
Daniel <DaniGwen@users.noreply.github.com>
2019-11-30 11:47:35 +0300

    Update
    Added Views and Controllers

--- Panda/Panda.App/Controllers/PackagesController.cs
+++ Panda/Panda.App/Controllers/PackagesController.cs
@@ -7,6 +7,7 @@ using Panda.Data;
 using Panda.Domein;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Panda.App.Controllers
@@ -58,18 +59,45 @@ namespace Panda.App.Controllers
 
             foreach (var package in packagesDb)
             {
-                var packageForViewModel = new PackagePendingViewModel
+                var packageViewModel = new PackagePendingViewModel
                 {
+                    Id = package.Id,
                     Description = package.Description,
                     Recipient = package.Recipient.UserName,
                     ShippingAddress = package.ShippingAddress,
                     Weight = package.Weight
                 };
 
-                viewModelPackages.Add(packageForViewModel);
+                viewModelPackages.Add(packageViewModel);
             }
 
             return this.View(viewModelPackages);
+        }
+
+        [HttpGet]
+        public IActionResult Ship(string id)
+        {
+            Package package = this.context.Packages.Find(id);
+            package.EstimatedDeliveryDate = DateTime.UtcNow
+                                                    .AddDays(new Random().Next(20, 40));
+            package.Status = this.context.PackageStatus
+                                         .SingleOrDefault(status => status.Name == "Shipped");
+            this.context.Update(package);
+            this.context.SaveChanges();
+
+            return this.Redirect("/Packages/Shipped");
+        }
+
+        [HttpGet]
+        public IActionResult Deliver(string id)
+        {
+            Package package = this.context.Packages.Find(id);
+            package.Status = this.context.PackageStatus
+                                         .SingleOrDefault(status => status.Name == "Delivered");
+            this.context.Update(package);
+            this.context.SaveChanges();
+
+            return this.Redirect("/Packages/Delivered");
         }
 
         [HttpGet]
@@ -84,15 +112,17 @@ namespace Panda.App.Controllers
 
             foreach (var package in packagesDb)
             {
-                var packageForViewModel = new PackageShippedViewModel
+                var packageViewModel = new PackageShippedViewModel
                 {
+                    Id = package.Id,
                     Description = package.Description,
                     Recipient = package.Recipient.UserName,
-                    DeliveryDate = DateTime.UtcNow.AddDays(20).ToString("dd/m/yyyy"),
+                    DeliveryDate = package.EstimatedDeliveryDate?
+                                          .ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
                     Weight = package.Weight
                 };
 
-                viewModelPackages.Add(packageForViewModel);
+                viewModelPackages.Add(packageViewModel);
             }
 
             return this.View(viewModelPackages);
@@ -110,7 +140,7 @@ namespace Panda.App.Controllers
 
             foreach (var package in packagesDb)
             {
-                var packageForViewModel = new PackageDeliveredViewModel
+                var packageViewModel = new PackageDeliveredViewModel
                 {
                     Description = package.Description,
                     Recipient = package.Recipient.UserName,
@@ -118,7 +148,7 @@ namespace Panda.App.Controllers
                     Weight = package.Weight
                 };
 
-                viewModelPackages.Add(packageForViewModel);
+                viewModelPackages.Add(packageViewModel);
             }
 
             return this.View(viewModelPackages);
