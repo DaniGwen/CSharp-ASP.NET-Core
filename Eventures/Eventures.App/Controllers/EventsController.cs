1487872e110f1b790640df97bfbeacf98c8e937f
Daniel <DaniGwen@users.noreply.github.com>
2019-12-08 21:28:06 +0300

    Added actions HttpPost/ HttpGetCreate

--- Eventures/Eventures.App/Controllers/EventsController.cs
+++ Eventures/Eventures.App/Controllers/EventsController.cs
@@ -1,5 +1,6 @@
 ﻿using Eventures.App.Areas.Identity.Data;
 using Eventures.App.Data;
+using Eventures.App.Models.BindingModels;
 using Eventures.App.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -13,6 +14,35 @@ namespace Eventures.App.Controllers
         public EventsController(EventuresDbContext context)
         {
             this.context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(EventCreateBindingModel bindingModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                Event eventForDb = new Event
+                {
+                    Name = bindingModel.Name,
+                    End = bindingModel.End,
+                    Place = bindingModel.Place,
+                    Start = bindingModel.Start,
+                    PricePerTicket = bindingModel.PricePerTicket,
+                    TotalTickets = bindingModel.TotalTickets
+                };
+
+                this.context.Events.Add(eventForDb);
+                this.context.SaveChanges();
+
+                return RedirectToAction("All");
+            }
+            return this.View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
         }
 
         public IActionResult All()
--- Eventures/Eventures.App/Models/BindingModels/EventCreateBindingModel.cs
+++ Eventures/Eventures.App/Models/BindingModels/EventCreateBindingModel.cs
@@ -13,25 +13,22 @@ namespace Eventures.App.Models.BindingModels
         [Display(Name = "Place")]
         public string Place { get; set; }
 
-
         [Required]
         [Display(Name = "Start")]
         public DateTime Start { get; set; }
-
 
         [Required]
         [Display(Name = "End")]
         public DateTime End { get; set; }
 
-
         [Required]
         [Display(Name = "Total tickets")]
+        [Range(0, int.MaxValue , ErrorMessage = "Number must be positive")]
         public int TotalTickets { get; set; }
-
 
         [Required]
         [Display(Name = "Price per ticket")]
-        [Range(0.0, double.MaxValue, ErrorMessage = "Price per ticket must be positive number")]
+        [Range(typeof(decimal), "0.00", "79228162514264337593543950335M", ErrorMessage = "Price per ticket must be positive number")]
         public decimal PricePerTicket { get; set; }
     }
 }
