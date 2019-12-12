3a91875a5abd7d84246fb6a27f69625664c425fb
Daniel <DaniGwen@users.noreply.github.com>
2019-12-12 09:22:19 +0300

    Added Validation to UserCreateBindingModel

--- Eventures/Eventures.App/Models/BindingModels/UserCreateBindingModel.cs
+++ Eventures/Eventures.App/Models/BindingModels/UserCreateBindingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,29 @@ namespace Eventures.App.Models.BindingModels
 {
     public class UserCreateBindingModel
     {
-        public string Username { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must have more than 3 letters", MinimumLength = 3)]
+        [RegularExpression("[A-Za-z0-9_`'-\\.]")]
+        public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must have more than 3 letters", MinimumLength = 3)]
+        [RegularExpression("[A-Za-z0-9_`'-\\.]")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
         public string Password { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
         public string ConfirmPassword { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
 
-        public string UCN { get; set }
+        [RegularExpression("^[0-9]{10}$")]
+        public string UCN { get; set; }
+
     }
 }
