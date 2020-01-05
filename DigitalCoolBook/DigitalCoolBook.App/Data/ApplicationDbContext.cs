e03b00a43240bff15cfbc84381a3cf7896e1265a
Daniel G <dani.gwenski@gmail.com>
2020-01-05 23:09:24 +0200

    Added DbSets

--- DigitalCoolBook/DigitalCoolBook.App/Data/ApplicationDbContext.cs
+++ DigitalCoolBook/DigitalCoolBook.App/Data/ApplicationDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DigitalCoolBook.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +6,22 @@ namespace DigitalCoolBook.App.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Attendance> Attendances { get; set; }
+
+        public DbSet<GradeParalelo> GradeParalelos { get; set; }
+
+        public DbSet<Grade> Grades { get; set; }
+
+        public DbSet<ScoreRecord> ScoreRecords { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<SubjectGrade> SubjectGrades { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
