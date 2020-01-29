6d232e552cb3af287a43bddf79c98fe9e7cf362a
Daniel G <dani.gwenski@gmail.com>
2020-01-29 08:24:05 +0200

    Created ParaleloViewModel and CreateParalelo action

---
+++ DigitalCoolBook/DigitalCoolBook.App/Models/GradeParaleloViewModels/ParaleloViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalCoolBook.App.Models.GradeParaleloViewModels
+{
+    public class ParaleloViewModel
+    {
+    }
+}
