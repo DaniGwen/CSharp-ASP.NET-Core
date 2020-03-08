5393f71ce2ff189f282d70d402301e032500b6ad
Daniel N <DaniGwen@users.noreply.github.com>
2020-03-08 13:26:33 +0200

    Added validation attributes in TeacherDetailsViewModel

--- DigitalCoolBook/DigitalCoolBook.App/Models/TeacherViewModels/TeacherDetailsViewModel.cs
+++ DigitalCoolBook/DigitalCoolBook.App/Models/TeacherViewModels/TeacherDetailsViewModel.cs
@@ -1,19 +1,25 @@
 ﻿namespace DigitalCoolBook.App.Models.TeacherViewModels
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class TeacherDetailsViewModel
     {
         public string Id { get; set; }
 
+        [Required(ErrorMessage="Попълнете полето.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Попълнете полето.")]
         public string Email { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Попълнете полето.")]
+        public string DateOfBirth { get; set; }
 
+        [Required(ErrorMessage = "Попълнете полето.")]
         public string PlaceOfBirth { get; set; }
 
+        [Required(ErrorMessage = "Попълнете полето.")]
         public string Sex { get; set; }
 
         public int? MobilePhone { get; set; }
