79acdb6ffef6b1b70458d5f2e7146b3172e012b7
Daniel N <DaniGwen@users.noreply.github.com>
2020-03-30 18:37:04 +0300

    Major structural changes
    Moved DigitalCoolBook.Data and DigitalCoolBook.Models In Data folder
    
    Moved DigitalCoolBook.App in Web folder
    
    Moved DigitalCoolBook.Web.Models in Web folder
    
    Moved DigitalCoolBook.Services and DigitalCoolBook.Services.Message in Service Folder

---
+++ DigitalCoolBook/Web/DigitalCoolBook.Web.Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace DigitalCoolBook.App.Models
+{
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Message { get; set; }
+    }
+}
