1af2dd96f11ddb4adfcfae66eec387d558689236
Daniel N <DaniGwen@users.noreply.github.com>
2020-04-23 11:56:06 +0300

    Added Entity Trip

--- SharedTrip/SharedTrip/Models/Trip.cs
+++ SharedTrip/SharedTrip/Models/Trip.cs
@@ -1,6 +1,21 @@
-﻿namespace SharedTrip.Models
+﻿using System;
+
+namespace SharedTrip.Models
 {
     public class Trip
     {
+        public string Id { get; set; }
+
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public UserTrip UserTrip { get; set; }
     }
 }
