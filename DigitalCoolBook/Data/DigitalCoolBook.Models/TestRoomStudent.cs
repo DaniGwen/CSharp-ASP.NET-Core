a65e92a48d87a22a0ca0ef377c3ab3edf6772993
Daniel N <DaniGwen@users.noreply.github.com>
2020-06-07 10:58:16 +0300

    Added view models Added Partial views

---
+++ DigitalCoolBook/Data/DigitalCoolBook.Models/TestRoomStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigitalCoolBook.Models
+{
+    public class TestRoomStudent
+    {
+        public TestRoomStudent()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        public string StudentId { get; set; }
+
+        public string TestRoomId { get; set; }
+
+        [ForeignKey("TestRoomId")]
+        public TestRoom TestRoom { get; set; }
+
+        public bool Finished { get; set; }
+    }
+}
