c94a2fa5c6ae0e175e17cc9d3487bbc472eaf3d1
Daniel N <DaniGwen@users.noreply.github.com>
2020-07-15 23:55:35 +0300

    Started new project RentCargoBus. Template set up. ApplicationDbContext set up. Added entity VanDetails.

---
+++ RentCargoBus/RentCargoBus.Data.Models/VanDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RentCargoBus.Data.Models
+{
+    public class VanDetails
+    {
+        public int BusId { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Name { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int MaxLoad { get; set; }
+
+        public decimal HirePrice { get; set; }
+    }
+}
