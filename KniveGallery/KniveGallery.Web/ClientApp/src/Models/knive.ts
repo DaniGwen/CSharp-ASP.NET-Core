cb5026d0581284c26199e92094a01a9263530116
Daniel N <DaniGwen@users.noreply.github.com>
2020-10-05 22:56:03 +0300

    Update

---
+++ KniveGallery/KniveGallery.Web/ClientApp/src/Models/knive.ts
@@ -0,0 +1,10 @@
+export interface Knive {
+  kniveId: number;
+  kniveName: string;
+  type: string;
+  length: number;
+  edgeLength: number;
+  handleType: string;
+  bladeType: string;
+  price: number;
+}
---
+++ KniveGallery/KniveGallery.Web/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { KnivesService } from '../../Services/knives.service';
+import { HttpClient } from '@angular/common/http';
+import { Knive } from '../../Models/knive';
+import { Observable } from 'rxjs';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+  knives: Knive[];
+  imagePath: string;
+  public isAuthenticated: Observable<boolean>;
+
+  constructor(private authorizeService: AuthorizeService,public http: HttpClient, private knivesService: KnivesService) { }
+
+  public ngOnInit() {
+    this.knivesService.getAllknives().subscribe((data: any) => {
+      this.knives = data;
+    });
+
+    this.isAuthenticated = this.authorizeService.isAuthenticated();
+  }
+}
