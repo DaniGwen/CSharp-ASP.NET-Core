29c19d164b0284ad28c1193787474ebad6627e32
Daniel N <DaniGwen@users.noreply.github.com>
2020-10-20 21:32:48 +0300

    KniveType.cs, custom.js added

---
+++ KniveGallery/KniveGallery.Web/Models/Enums/KniveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KniveGallery.Web.Models.Enums
+{
+    public enum KniveType
+    {
+        MiddleClass = 1,
+        HighClass = 2,
+        KitchenClass = 3
+    }
+}
