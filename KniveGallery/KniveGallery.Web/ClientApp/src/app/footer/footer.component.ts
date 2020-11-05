d70bc4d3311ea20d9b83c5f90dd450fe189dc61a
Daniel <dani.gwenski@users.noreply.github.com>
2020-11-05 16:52:19 +0200

    Update

--- KniveGallery/KniveGallery.Web/ClientApp/src/app/footer/footer.component.ts
+++ KniveGallery/KniveGallery.Web/ClientApp/src/app/footer/footer.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { KnivesService } from '../../Services/knives.service'
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 
 export class FooterComponent {
 
-    public adminEmail: string[];
-    public adminPhone: string;
+  public adminEmail: string[];
+  public adminPhone: string;
 
-    constructor(private knivesService: KnivesService) {
-    }
+  constructor(private knivesService: KnivesService) {
+  }
 
-    ngOnInit() {
-        this.knivesService.getAdminDetails().subscribe((data: any)=>{
-            this.adminEmail = data.phoneNumber,
-            this.adminPhone = data.email
-        });
-    }
+  ngOnInit() {
+    this.knivesService.getAdminDetails().subscribe((data: any) => {
+      this.adminEmail = data.email,
+        this.adminPhone = data.phoneNumber
+    });
+  }
 }
